93e5f572d17df33375d90e0feb752948108f9969
Lepleen <leticiachavescs@gmail.com>
2024-06-24 00:17:43 -0300

    update

--- index.js
+++ index.js
@@ -41,3 +41,55 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+
+
+//* CODIGO HOVER CABECALHO */
+ //pega os elementos pelo DOM
+ const hero = document.querySelector(".hero");
+ const myimages = document.querySelectorAll(".myimages img");
+
+ //configura a sensibilidade de cada imagem
+ const sensitivities = [
+   55, -50, 40
+ ];
+
+ //loop por todas as imagens
+ for(let i = 0; i < myimages.length; i++){
+   hero.addEventListener("mousemove", e => {
+     setTimeout(() => {
+       const x = e.clientX;
+       const y = e.clientY;
+
+
+       const w = hero.offsetWidth / 2;
+       const h = hero.offsetHeight / 2;
+
+
+       let convertX = ((x - w) * sensitivities[i]) / w;
+       let convertY = ((y - h) * sensitivities[i]) / h;
+
+       myimages[i].style.transform = `
+       translateX(${convertX}px)
+       translateY(calc(-50% - ${convertY}px)`;
+
+     }, 200);
+   });
+ };
+
+
+
+ const scrollDivs = document.querySelectorAll('.js-scroll');
+
+ function animaScroll(){
+  scrollDivs.forEach(divs => {
+    const divTop = divs.getBoundingClientRect().top;
+    if(divTop < 0 ){
+      divs.classList.add('ativo');
+    }
+  });
+}
+ 
+ window.addEventListener('scroll', animaScroll);
+
+
+ 
